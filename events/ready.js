fb49e317d4698c1583a24542d949a6e1102a1bd1
Norbig <48188925+NorbigYT@users.noreply.github.com>
2019-08-08 00:21:10 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,8 @@
 let prefix = 'your prefix here'
+//Make sure to change the prefix section to your own custom prefix!
 exports.run = async (client, message) => {
   console.log('Bot is online with username ' + client.user.username + "!");
   console.log(client.guilds.map(g=>`${g.name} (${g.id})`).join('\n'));
-  
   
   client.user.setActivity(`Prefix: ${prefix}`, { type: 'PLAYING'});
   //You always can change the bot's activity!
