d2bff2de46510a85305e90163d9f83418fa618e5
Norbig <48188925+NorbigYT@users.noreply.github.com>
2019-08-08 00:19:45 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,11 @@
+let prefix = 'your prefix here'
+exports.run = async (client, message) => {
+  console.log('Bot is online with username ' + client.user.username + "!");
+  console.log(client.guilds.map(g=>`${g.name} (${g.id})`).join('\n'));
+  
+  
+  client.user.setActivity(`Prefix: ${prefix}`, { type: 'PLAYING'});
+  //You always can change the bot's activity!
+  //To show the servers count: ${client.guilds.size}
+  //To show the total users count: ${client.users.size}
+}
